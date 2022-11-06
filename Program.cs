7b14236f77b7b24ddf6c1d3d6d1ba23c5a03fdaa
Claudiomiro Marques <marques_55@hotmail.com>
2022-11-06 17:44:25 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using ProjetoHospedagem.Models;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+List<Pessoa> hospedes = new List<Pessoa>();
+
+Pessoa p1 = new Pessoa(nome: "Hóspede 1", sobrenome: "Claudio");
+Pessoa p2 = new Pessoa(nome: "Hóspede 2", sobrenome: "Cristal");
+Pessoa p3 = new Pessoa(nome: "Hóspede 3", sobrenome: "Gabi");
+//Pessoa p4 = new Pessoa(nome: "Hóspede 4", sobrenome: "Mag");
+//Pessoa p5 = new Pessoa(nome: "Hóspede 3", sobrenome: "");
+//Pessoa p6 = new Pessoa(nome: "Hóspede 4", sobrenome: "");
+
+hospedes.Add(p1);
+hospedes.Add(p2);
+hospedes.Add(p3);
+//hospedes.Add(p4);
+//hospedes.Add(p5);
+//hospedes.Add(p6);
+
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 4, valorDiaria: 50);
+
+Reserva reserva = new Reserva(diasReservados: 10);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor diária: R$ {reserva.CalcularValorDiaria()}");
